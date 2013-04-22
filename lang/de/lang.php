6adbf5017a708aa6edf897551c78ff7389b708c1
Leo Eibler <github@sprossenwanne.at>
2013-04-22 10:18:47 +0200

    add german language file

---
+++ lang/de/lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Leo Eibler <dokuwiki@sprossenwanne.at>
+ * @date 20130405 Leo Eibler <dokuwiki@sprossenwanne.at> \n
+ *                create german language file \n
+ */
+ 
+// custom language strings for the plugin
+$lang['qb_todobutton'] = 'Markierten Text als Aufgabe/Todo';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/de/settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * English language strings for the ToDo Plugin
+ *
+ * @author  Leo Eibler <dokuwiki@sprossenwanne.at>
+ * @date 20130405 Leo Eibler <dokuwiki@sprossenwanne.at> \n
+ *                create german language file \n
+ */
+ 
+$lang['AllowLinks'] = 'Sollen Aufgaben/Todos auch auf Seiten mit dem gleichen Namen verlinken?';
+$lang['ActionNamespace'] = 'Wenn AllowLinks aktiv ist, mit welchem Namespace sollen die Aufgaben-Seiten erstellt werden (".:" = Aktueller NS, leer = Root NS)';
+$lang['Strikethrough'] = 'Sollen erledigte Aufgaben durchgestrichen werden?';
+$lang['CheckboxText'] = 'Wenn AllowLinks nicht aktiv ist, soll dann ein Klick auf die Aufgabe, die Aufgabe als erledigt markieren?';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
