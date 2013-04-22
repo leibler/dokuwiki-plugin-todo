76bd199b58b35a0352cdde848767e36eb0abc464
Leo Eibler <github@sprossenwanne.at>
2013-04-22 11:24:04 +0200

    bugfix: use chk.is() by Christian; bugfix: config option Strikethrough

--- script.js
+++ script.js
@@ -7,14 +7,24 @@
 **                change only the clicked todo item instead of all items with the same text \n
 ** @date 20130408 Leo Eibler <dokuwiki@sprossenwanne.at> \n
 **                migrate changes made by Christian Marg to current version of plugin (use jQuery) \n
-** @date 20130410 by Leo Eibler <dokuwiki@sprossenwanne.at> / http://www.eibler.at
-**                bugfix: encoding html code (security risk <todo><script>alert('hi')</script></todo>) - bug reported by Andreas
+** @date 20130410 by Leo Eibler <dokuwiki@sprossenwanne.at> / http://www.eibler.at \n
+**                bugfix: encoding html code (security risk <todo><script>alert('hi')</script></todo>) - bug reported by Andreas \n
+** @date 20130413 Christian Marg <marg@rz.tu-clausthal.de> \n
+**                bugfix: chk.attr('checked') returns checkbox state from html - use chk.is(':checked') - see http://www.unforastero.de/jquery/checkbox-angehakt.php \n
+** @date 20130413 by Leo Eibler <dokuwiki@sprossenwanne.at> / http://www.eibler.at \n
+**                bugfix: config option Strikethrough \n
 */
 function whenCompleted( data ){
 	//alert(data);
 }
 
-function clickSpan( span, id ){
+/*
+** @brief onclick method for span element
+** @param span	the jQuery span element
+** @param id	string the page
+** @param strike	int strikethrough activated (1) or not (0) - see config option Strikethrough
+*/
+function clickSpan( span, id, strike ){
 	//Find the checkbox node we need
 	var chk;
 	//var preve = jQuery(span).prev();
@@ -27,13 +37,7 @@ function clickSpan( span, id ){
 		preve = preve.prev();
 	}
 	if( chk.is("input") ) {
-		chk.attr('checked', !chk.attr('checked'));
-		var strike;
-		if( chk.attr('checked') ) {
-			strike = true;
-		} else {
-			strike = false;
-		}
+		chk.attr('checked', !chk.is(':checked'));
 		todo( chk, id, strike );
 		//chk.checked = !chk.checked;
 	} else {
@@ -42,6 +46,12 @@ function clickSpan( span, id ){
 
 }
 
+/*
+** @brief onclick method for input element
+** @param chk	the jQuery input element
+** @param path	string the page
+** @param strike	int strikethrough activated (1) or not (0) - see config option Strikethrough
+*/
 function todo( chk, path, strike ){
 
 	/*
@@ -54,7 +64,7 @@ function todo( chk, path, strike ){
 	*/ 
 
 	//chk = jQuery(chk);
-	//console.log( "got chk with data-id='"+chk.attr("data-index")+"'" );
+	//console.log( "got chk with data-id='"+chk.attr("data-index")+"' chk.is(':checked')='"+chk.is(':checked')+"'" );
 	var inputTodohiddentext = chk.nextAll("span.todotext").children("input.todohiddentext").first();
 	var spanTodoinnertext = chk.nextAll("span.todotext").children("span.todoinnertext").first();
 	var _postVarIndex;
@@ -66,9 +76,13 @@ function todo( chk, path, strike ){
 		_postVarIndex = -1;
 	}
 	if( spanTodoinnertext && inputTodohiddentext ) {
-		if( chk.attr('checked') ) {
+		//if( chk.attr('checked') ) {
+		// @date 20130413 Christian bugfix chk.attr('checked') returns checkbox state from html - use chk.is(':checked') - see http://www.unforastero.de/jquery/checkbox-angehakt.php
+		if( chk.is(':checked') ) {
 			_postVarChecked = "1";
-			spanTodoinnertext.html( "<del>"+decodeURIComponent( inputTodohiddentext.val().replace(/\+/g, " ") ).replace(/</g, "&lt;").replace(/>/g, "&gt;")+"</del>" );
+			if( strike ) {
+				spanTodoinnertext.html( "<del>"+decodeURIComponent( inputTodohiddentext.val().replace(/\+/g, " ") ).replace(/</g, "&lt;").replace(/>/g, "&gt;")+"</del>" );
+			}
 		} else {
 			_postVarChecked = "0";
 			spanTodoinnertext.html( decodeURIComponent( inputTodohiddentext.val().replace(/\+/g, " ") ).replace(/</g, "&lt;").replace(/>/g, "&gt;") );
