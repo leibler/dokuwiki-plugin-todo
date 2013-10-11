562b2a1d6c7030c7009ac82e42082302901c7cd8
Gerrit Uitslag <klapinklapin@gmail.com>
2013-10-11 13:46:46 +0200

    Refactor javasript, use ToDoPlugin namespace

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  * @date 20130407 Leo Eibler <dokuwiki@sprossenwanne.at> \n
  *                use jQuery for finding the elements \n
  * @date 20130408 Christian Marg <marg@rz.tu-clausthal.de> \n
- *                change only the clicked todo item instead of all items with the same text \n
+ *                change only the clicked todoitem instead of all items with the same text \n
  * @date 20130408 Leo Eibler <dokuwiki@sprossenwanne.at> \n
  *                migrate changes made by Christian Marg to current version of plugin (use jQuery) \n
  * @date 20130410 by Leo Eibler <dokuwiki@sprossenwanne.at> / http://www.eibler.at \n
@@ -25,108 +25,87 @@
  * ---anchor with text or text only
  */
 
-/**
- * lock to prevent simultanous requests
- */
-var todoplugin_locked = { clickspan: false, todo: false };
+var ToDoPlugin = {
+    /**
+     * lock to prevent simultanous requests
+     */
+    locked: false,
 
-/**
- * @brief onclick method for span element
- *
- * @param {jQuery} $span  the jQuery span element
- * @param {string} id     the page
- * @param {int}    strike strikethrough activated (1) or not (0) - see config option Strikethrough
- */
-function clickSpan($span, id, strike) {
-    //skip when locked
-    if (todoplugin_locked.clickspan || todoplugin_locked.todo) {
-        return;
-    }
-    //set lock
-    todoplugin_locked.clickspan = true;
-
-    //Find the checkbox node we need
-    var $chk = $span.prevAll('input.todocheckbox:first');
-
-    if ($chk.is("input")) {
-        $chk.prop('checked', !$chk.is(':checked'));
-        todo($chk, id, strike);
-    } else {
-        alert("Appropriate javascript element not found.");
-    }
-
-}
-
-/**
- * @brief onclick method for input element
- *
- * @param {jQuery} $chk    the jQuery input element
- * @param {string} path    the page
- * @param {int}    strike  strikethrough activated (1) or not (0) - see config option Strikethrough
- */
-function todo($chk) {
-    //skip when locked
-    if (todoplugin_locked.todo) {
-        return;
-    }
-    //set lock
-    todoplugin_locked.todo = true;
+    /**
+     * @brief onclick method for input element
+     *
+     * @param {jQuery} $chk the jQuery input element
+     */
+    todo: function ($chk) {
+        //skip when locked
+        if (ToDoPlugin.locked) {
+            return;
+        }
+        //set lock
+        ToDoPlugin.locked = true;
 
 
+        var $spanTodoinnertext = $chk.nextAll("span.todotext:first").find("span.todoinnertext"),
+            param = $chk.data(), // contains: index, pageid, date, strikethrough
+            checked = !$chk.is(':checked');
 
-    var $spanTodoinnertext = $chk.nextAll("span.todotext:first").find("span.todoinnertext"),
-        param = $chk.data(), // contains: index, pageid, date, strikethrough
-        checked = !$chk.is(':checked');
+        // if the data-index attribute is set, this is a call from the page where the todos are defined
+        if (param.index === undefined) param.index = -1;
 
-    // if the data-index attribute is set, this is a call from the page where the todos are defined
-    if (param.index === undefined) param.index = -1;
+        if ($spanTodoinnertext.length) {
 
-    if ($spanTodoinnertext.length) {
+            /**
+             * Callback function update the todoitem when save request succeed
+             *
+             * @param {Array} data returned by ajax request
+             */
+            var whenCompleted = function (data) {
+                //update date after edit and show alert when needed
+                if (data.date) {
+                    jQuery('input.todocheckbox').data('date', data.date);
+                }
+                if (data.message) {
+                    alert(data.message);
+                }
+                //apply styling, or undo checking checkbox
+                if (data.succeed) {
+                    $chk.prop('checked', checked);
 
-        var whenCompleted = function (data) {
-            //update date after edit and show alert when needed
-            if (data.date) {
-                jQuery('input.todocheckbox').data('date', data.date);
-            }
-            if (data.message) {
-                alert(data.message);
-            }
-            //apply styling, or undo checking checkbox
-            if (data.succeed) {
-                $chk.prop('checked', checked);
-
-                if (checked) {
-                    if (param.strikethrough && !$spanTodoinnertext.parent().is("del")) {
-                        $spanTodoinnertext.wrap("<del></del>");
-                    }
-                } else {
-                    if ($spanTodoinnertext.parent().is("del")) {
-                        $spanTodoinnertext.unwrap();
+                    if (checked) {
+                        if (param.strikethrough && !$spanTodoinnertext.parent().is("del")) {
+                            $spanTodoinnertext.wrap("<del></del>");
+                        }
+                    } else {
+                        if ($spanTodoinnertext.parent().is("del")) {
+                            $spanTodoinnertext.unwrap();
+                        }
                     }
                 }
-            }
 
-            //release lock
-            todoplugin_locked = { clickspan: false, todo: false };
-        };
+                //release lock
+                ToDoPlugin.locked = false;
+            };
 
-        jQuery.post(
-            DOKU_BASE + 'lib/exe/ajax.php',
-            {
-                call: 'plugin_todo',
-                index: param.index,
-                path: param.pageid,
-                checked: checked ? "1" : "0",
-                date: param.date
-            },
-            whenCompleted,
-            'json'
-        );
-    } else {
-        alert("Appropriate javascript element not found.\nReverting checkmark.");
+            jQuery.post(
+                DOKU_BASE + 'lib/exe/ajax.php',
+                {
+                    call: 'plugin_todo',
+                    index: param.index,
+                    path: param.pageid,
+                    checked: checked ? "1" : "0",
+                    date: param.date
+                },
+                whenCompleted,
+                'json'
+            );
+        } else {
+            alert("Appropriate javascript element not found.\nReverting checkmark.");
+        }
+
     }
 
-}
+
+};
 
 jQuery(function(){
 
@@ -139,7 +118,7 @@ jQuery(function(){
         // undo checking the checkbox
         $this.prop('checked', !$this.is(':checked'));
 
-        todo($this);
+        ToDoPlugin.todo($this);
     });
 
     // add click handler to todotext spans when marked with 'clickabletodo'
@@ -147,7 +126,7 @@ jQuery(function(){
         //Find the checkbox node we need
         var $chk = jQuery(this).prevAll('input.todocheckbox:first');
 
-        todo($chk);
+        ToDoPlugin.todo($chk);
     });
 
 });
