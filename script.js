aa11d6a46e34fdcba785525ca93431f9f30b4c89
leibler <github@sprossenwanne.at>
2014-03-17 19:47:07 +0100

    add descriptive summary for check or uncheck todo

--- script.js
+++ script.js
@@ -1,104 +1,132 @@
-/*
-** @date 20130405 Leo Eibler <dokuwiki@sprossenwanne.at> \n
-**                replace old sack() method with new jQuery method and use post instead of get - see https://www.dokuwiki.org/devel:jqueryfaq \n
-** @date 20130407 Leo Eibler <dokuwiki@sprossenwanne.at> \n
-**                use jQuery for finding the elements \n
-** @date 20130408 Christian Marg <marg@rz.tu-clausthal.de> \n
-**                change only the clicked todo item instead of all items with the same text \n
-** @date 20130408 Leo Eibler <dokuwiki@sprossenwanne.at> \n
-**                migrate changes made by Christian Marg to current version of plugin (use jQuery) \n
-** @date 20130410 by Leo Eibler <dokuwiki@sprossenwanne.at> / http://www.eibler.at \n
-**                bugfix: encoding html code (security risk <todo><script>alert('hi')</script></todo>) - bug reported by Andreas \n
-** @date 20130413 Christian Marg <marg@rz.tu-clausthal.de> \n
-**                bugfix: chk.attr('checked') returns checkbox state from html - use chk.is(':checked') - see http://www.unforastero.de/jquery/checkbox-angehakt.php \n
-** @date 20130413 by Leo Eibler <dokuwiki@sprossenwanne.at> / http://www.eibler.at \n
-**                bugfix: config option Strikethrough \n
-*/
-function whenCompleted( data ){
-	//alert(data);
-}
+/**
+ * @date 20130405 Leo Eibler <dokuwiki@sprossenwanne.at> \n
+ *                replace old sack() method with new jQuery method and use post instead of get - see https://www.dokuwiki.org/devel:jqueryfaq \n
+ * @date 20130407 Leo Eibler <dokuwiki@sprossenwanne.at> \n
+ *                use jQuery for finding the elements \n
+ * @date 20130408 Christian Marg <marg@rz.tu-clausthal.de> \n
+ *                change only the clicked todoitem instead of all items with the same text \n
+ * @date 20130408 Leo Eibler <dokuwiki@sprossenwanne.at> \n
+ *                migrate changes made by Christian Marg to current version of plugin (use jQuery) \n
+ * @date 20130410 by Leo Eibler <dokuwiki@sprossenwanne.at> / http://www.eibler.at \n
+ *                bugfix: encoding html code (security risk <todo><script>alert('hi')</script></todo>) - bug reported by Andreas \n
+ * @date 20130413 Christian Marg <marg@rz.tu-clausthal.de> \n
+ *                bugfix: chk.attr('checked') returns checkbox state from html - use chk.is(':checked') - see http://www.unforastero.de/jquery/checkbox-angehakt.php \n
+ * @date 20130413 by Leo Eibler <dokuwiki@sprossenwanne.at> / http://www.eibler.at \n
+ *                bugfix: config option Strikethrough \n
+ */
 
-/*
-** @brief onclick method for span element
-** @param span	the jQuery span element
-** @param id	string the page
-** @param strike	int strikethrough activated (1) or not (0) - see config option Strikethrough
-*/
-function clickSpan( span, id, strike ){
-	//Find the checkbox node we need
-	var chk;
-	//var preve = jQuery(span).prev();
-	var preve = span.prev();
-	while( preve ) {
-		if( preve.is("input") ) {
-			chk = preve;
-			break;
-		}
-		preve = preve.prev();
-	}
-	if( chk.is("input") ) {
-		chk.attr('checked', !chk.is(':checked'));
-		todo( chk, id, strike );
-		//chk.checked = !chk.checked;
-	} else {
-		alert("Appropriate javascript element not found.");
-	}
+/**
+ * html-layout:
+ *
+ * +input[checkbox].todocheckbox
+ * +span.todotext
+ * -del
+ * --span.todoinnertext
+ * ---anchor with text or text only
+ */
 
-}
+var ToDoPlugin = {
+    /**
+     * lock to prevent simultanous requests
+     */
+    locked: false,
 
-/*
-** @brief onclick method for input element
-** @param chk	the jQuery input element
-** @param path	string the page
-** @param strike	int strikethrough activated (1) or not (0) - see config option Strikethrough
-*/
-function todo( chk, path, strike ){
+    /**
+     * @brief onclick method for input element
+     *
+     * @param {jQuery} $chk the jQuery input element
+     */
+    todo: function ($chk) {
+        //skip when locked
+        if (ToDoPlugin.locked) {
+            return;
+        }
+        //set lock
+        ToDoPlugin.locked = true;
 
-	/*
-	** +Checkbox
-	** +Span
-	** -Hidden
-	** -Span
-	** --Anchor  
-	** ---Del
-	*/ 
 
-	//chk = jQuery(chk);
-	//console.log( "got chk with data-id='"+chk.attr("data-index")+"' chk.is(':checked')='"+chk.is(':checked')+"'" );
-	var inputTodohiddentext = chk.nextAll("span.todotext").children("input.todohiddentext").first();
-	var spanTodoinnertext = chk.nextAll("span.todotext").children("span.todoinnertext").first();
-	var _postVarIndex;
-	if( chk.attr("data-index") !== undefined ) {
-		// if the data-index attribute is set, this is a call from the page where the todos are defined
-		_postVarIndex = chk.attr("data-index");
-	} else {
-		// if the data-index attribute is not set, this is a call from searchpattern dokuwiki plugin rendered page
-		_postVarIndex = -1;
-	}
-	if( spanTodoinnertext && inputTodohiddentext ) {
-		//if( chk.attr('checked') ) {
-		// @date 20130413 Christian bugfix chk.attr('checked') returns checkbox state from html - use chk.is(':checked') - see http://www.unforastero.de/jquery/checkbox-angehakt.php
-		if( chk.is(':checked') ) {
-			_postVarChecked = "1";
-			if( strike ) {
-				spanTodoinnertext.html( "<del>"+decodeURIComponent( inputTodohiddentext.val().replace(/\+/g, " ") ).replace(/</g, "&lt;").replace(/>/g, "&gt;")+"</del>" );
-			}
-		} else {
-			_postVarChecked = "0";
-			spanTodoinnertext.html( decodeURIComponent( inputTodohiddentext.val().replace(/\+/g, " ") ).replace(/</g, "&lt;").replace(/>/g, "&gt;") );
-		}
-		jQuery.post(
-			DOKU_BASE+'lib/plugins/todo/ajax.php',
-			{ 
-				"index": _postVarIndex,
-				"path": path,
-				"checked": _postVarChecked,
-				"origVal": inputTodohiddentext.val().replace(/\+/g, " ")
-			},
-			whenCompleted
-		);
-	} else {
-		alert("Appropriate javascript element not found.\nReverting checkmark.");
-	}
+        var $spanTodoinnertext = $chk.nextAll("span.todotext:first").find("span.todoinnertext"),
+            param = $chk.data(), // contains: index, pageid, date, strikethrough
+            checked = !$chk.is(':checked');
 
-}
+        // if the data-index attribute is set, this is a call from the page where the todos are defined
+        if (param.index === undefined) param.index = -1;
+
+        if ($spanTodoinnertext.length) {
+
+            /**
+             * Callback function update the todoitem when save request succeed
+             *
+             * @param {Array} data returned by ajax request
+             */
+            var whenCompleted = function (data) {
+                //update date after edit and show alert when needed
+                if (data.date) {
+                    jQuery('input.todocheckbox').data('date', data.date);
+                }
+                if (data.message) {
+                    alert(data.message);
+                }
+                //apply styling, or undo checking checkbox
+                if (data.succeed) {
+                    $chk.prop('checked', checked);
+
+                    if (checked) {
+                        if (param.strikethrough && !$spanTodoinnertext.parent().is("del")) {
+                            $spanTodoinnertext.wrap("<del></del>");
+                        }
+                    } else {
+                        if ($spanTodoinnertext.parent().is("del")) {
+                            $spanTodoinnertext.unwrap();
+                        }
+                    }
+                }
+
+                //release lock
+                ToDoPlugin.locked = false;
+            };
+
+            jQuery.post(
+                DOKU_BASE + 'lib/exe/ajax.php',
+                {
+                    call: 'plugin_todo',
+                    index: param.index,
+                    pageid: param.pageid,
+                    checked: checked ? "1" : "0",
+                    date: param.date
+                },
+                whenCompleted,
+                'json'
+            );
+        } else {
+            alert("Appropriate javascript element not found.\nReverting checkmark.");
+        }
+
+    }
+
+
+};
+
+jQuery(function(){
+
+    // add handler to checkbox
+    jQuery('input.todocheckbox').click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        var $this = jQuery(this);
+        // undo checking the checkbox
+        $this.prop('checked', !$this.is(':checked'));
+
+        ToDoPlugin.todo($this);
+    });
+
+    // add click handler to todotext spans when marked with 'clickabletodo'
+    jQuery('span.todotext.clickabletodo').click(function(){
+        //Find the checkbox node we need
+        var $chk = jQuery(this).prevAll('input.todocheckbox:first');
+
+        ToDoPlugin.todo($chk);
+    });
+
+});
---
+++ syntax/list.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * DokuWiki Plugin todo_list (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+/**
+ * Class syntax_plugin_todo_list
+ */
+class syntax_plugin_todo_list extends syntax_plugin_todo_todo {
+
+    /**
+     * @return string Syntax mode type
+     */
+    public function getType() {
+        return 'substition';
+    }
+
+    /**
+     * @return string Paragraph type
+     */
+    public function getPType() {
+        return 'block';
+    }
+
+    /**
+     * @return int Sort order - Low numbers go before high numbers
+     */
+    public function getSort() {
+        return 250;
+    }
+
+    /**
+     * Connect lookup pattern to lexer.
+     *
+     * @param string $mode Parser mode
+     */
+    public function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('~~TODOLIST[^~]*~~', $mode, 'plugin_todo_list');
+    }
+
+    /**
+     * Handle matches of the todolist syntax
+     *
+     * @param string $match The match of the syntax
+     * @param int $state The state of the handler
+     * @param int $pos The position in the document
+     * @param Doku_Handler $handler The handler
+     * @return array Data for the renderer
+     */
+    public function handle($match, $state, $pos, Doku_Handler &$handler) {
+
+        $options = substr($match, 10, -2); // strip markup
+        $options = explode(' ', $options);
+        $data = array(
+            'completed' => 'all',
+            'assigned' => 'all'
+        );
+        $allowedvalues = array('yes', 'no');
+        foreach($options as $option) {
+            @list($key, $value) = explode(':', $option, 2);
+            switch($key) {
+                case 'completed':
+                    if(in_array($value, $allowedvalues)) {
+                        $data['completed'] = ($value == 'yes');
+                    }
+                    break;
+                case 'assigned':
+                    if(in_array($value, $allowedvalues)) {
+                        $data['assigned'] = ($value == 'yes');
+                        break;
+                    }
+                    //assigned?
+                    $data['assigned'] = explode(',', $value);
+                    $data['assigned'] = array_map(
+                        function ($user) {
+                            //placeholder
+                            if($user == '@@USER@@') {
+                                return $user;
+                            }
+                            //user
+                            return ltrim($user, '@');
+                        }, $data['assigned']
+                    );
+                    break;
+            }
+        }
+        return $data;
+    }
+
+    /**
+     * Render xhtml output or metadata
+     *
+     * @param string $mode Renderer mode (supported modes: xhtml)
+     * @param Doku_Renderer $renderer The renderer
+     * @param array $data The data from the handler() function
+     * @return bool If rendering was successful.
+     */
+    public function render($mode, Doku_Renderer &$renderer, $data) {
+        global $conf;
+
+        if($mode != 'xhtml') return false;
+        /** @var Doku_Renderer_xhtml $renderer */
+
+        $opts['pattern'] = '/<todo([^>]*)>(.*)<\/todo[\W]*?>/'; //all todos in a wiki page
+        //TODO check if storing subpatterns doesn't cost too much resources
+
+        // search(&$data, $base,            $func,                       $opts,$dir='',$lvl=1,$sort='natural')
+        search($todopages, $conf['datadir'], array($this, 'search_todos'), $opts); //browse wiki pages with callback to search_pattern
+
+        $todopages = $this->filterpages($todopages, $data);
+
+        $this->htmlTodoTable($renderer, $todopages);
+
+        return true;
+    }
+
+    /**
+     * Custom search callback
+     *
+     * This function is called for every found file or
+     * directory. When a directory is given to the function it has to
+     * decide if this directory should be traversed (true) or not (false).
+     * Return values for files are ignored
+     *
+     * All functions should check the ACL for document READ rights
+     * namespaces (directories) are NOT checked (when sneaky_index is 0) as this
+     * would break the recursion (You can have an nonreadable dir over a readable
+     * one deeper nested) also make sure to check the file type (for example
+     * in case of lockfiles).
+     *
+     * @param array &$data  - Reference to the result data structure
+     * @param string $base  - Base usually $conf['datadir']
+     * @param string $file  - current file or directory relative to $base
+     * @param string $type  - Type either 'd' for directory or 'f' for file
+     * @param int    $lvl   - Current recursion depht
+     * @param array  $opts  - option array as given to search()
+     * @return bool if this directory should be traversed (true) or not (false). Return values for files are ignored.
+     */
+    public function search_todos(&$data, $base, $file, $type, $lvl, $opts) {
+        $item['id'] = pathID($file); //get current file ID
+
+        //we do nothing with directories
+        if($type == 'd') return true;
+
+        //only search txt files
+        if(substr($file, -4) != '.txt') return true;
+
+        //check ACL
+        if(auth_quickaclcheck($item['id']) < AUTH_READ) return false;
+
+        $wikitext = rawWiki($item['id']); //get wiki text
+
+        $item['count'] = preg_match_all($opts['pattern'], $wikitext, $matches); //count how many times appears the pattern
+        if(!empty($item['count'])) { //if it appears at least once
+            $item['matches'] = $matches;
+            $data[] = $item;
+        }
+        return true;
+    }
+
+    /**
+     * filter the pages
+     *
+     * @param $todopages array pages with all todoitems
+     * @param $data      array listing parameters
+     * @return array filtered pages
+     */
+    private function filterpages($todopages, $data) {
+        $pages = array();
+        foreach($todopages as $page) {
+            $todos = array();
+            // contains 3 arrays: an array with complete matches and 2 arrays with subpatterns
+            foreach($page['matches'][1] as $todoindex => $todomatch) {
+                list($checked, $todouser) = $this->parseTodoArgs($todomatch);
+                $todotitle = trim($page['matches'][2][$todoindex]);
+
+                if($this->isRequestedTodo($data, $checked, $todouser)) {
+                    $todos[] = array($todotitle, $todoindex, $todouser, $checked);
+                }
+            }
+            if(count($todos) > 0) {
+                $pages[] = array('id' => $page['id'], 'todos' => $todos);
+            }
+        }
+        return $pages;
+    }
+
+    /**
+     * Create html for table with todos
+     *
+     * @param Doku_Renderer_xhtml $R
+     * @param array $todopages
+     */
+    private function htmlTodoTable($R, $todopages) {
+        $R->table_open();
+        foreach($todopages as $page) {
+            $R->tablerow_open();
+            $R->tableheader_open();
+            $R->internallink($page['id'], $page['id']);
+            $R->tableheader_close();
+            $R->tablerow_close();
+            foreach($page['todos'] as $todo) {
+                $R->tablerow_open();
+                $R->tablecell_open();
+                $R->doc .= $this->createTodoItem($R, $todo[0], $todo[1], $todo[2], $todo[3], $page['id']);
+                $R->tablecell_close();
+                $R->tablerow_close();
+            }
+        }
+        $R->table_close();
+    }
+
+    /**
+     * Check the conditions for adding a todoitem
+     *
+     * @param $data     array the defined filters
+     * @param $checked  bool completion status of task; true: finished, false: open
+     * @param $todouser string user username of user
+     * @return bool if the todoitem should be listed
+     */
+    private function isRequestedTodo($data, $checked, $todouser) {
+        //completion status
+        $condition1 = $data['completed'] === 'all' //all
+                      || $data['completed'] === $checked; //yes or no
+
+        // resolve placeholder in assignees
+        $requestedassignees = array();
+        if(is_array($data['assigned'])) {
+            $requestedassignees = array_map(
+                function($user) {
+                    if($user == '@@USER@@' && !empty($_SERVER['REMOTE_USER'])) {  //$INPUT->server->str('REMOTE_USER')
+                            return $_SERVER['REMOTE_USER'];
+                    }
+                    return $user;
+                },
+                $data['assigned']
+            );
+        }
+        //assigned
+        $condition2 =   $data['assigned'] === 'all' //all
+                        || (is_bool($data['assigned']) && $data['assigned'] == $todouser) //yes or no
+                        || (is_array($data['assigned']) && in_array($todouser, $requestedassignees)); //one of the requested users?
+
+        return $condition1 AND $condition2;
+    }
+}
