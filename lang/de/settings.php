51cdd5b88409f3c284bfa488ffdfe28b12629524
runout-at <markus@runout.at>
2014-05-11 23:41:09 +0200

    Update settings.php

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -11,8 +11,8 @@ $lang['AllowLinks'] = 'Sollen Aufgaben/Todos auch auf Seiten mit dem gleichen Na
 $lang['ActionNamespace'] = 'Wenn AllowLinks aktiv ist, mit welchem Namespace sollen die Aufgaben-Seiten erstellt werden (".:" = Aktueller NS, leer = Root NS)';
 $lang['Strikethrough'] = 'Sollen erledigte Aufgaben durchgestrichen werden?';
 $lang['CheckboxText'] = 'Wenn AllowLinks nicht aktiv ist, soll dann ein Klick auf die Aufgabe, die Aufgabe als erledigt markieren?';
-$lang['Checkbox'] = 'Soll in Listen die CheckBox aufscheinen?';
-$lang['Header'] = 'Wie soll der Kopf bei Listen beschriftet werden? Als "id" oder mit der ersten Kopfzeile der Seite "firstheader".';
-$lang['Username'] = 'Wie soll der Username ausgegeben werden? Als "username", voller Name "real" oder garnicht "none"';
+$lang['Checkbox'] = '(Default Wert für Option "checkbox") Soll in Listen die CheckBox aufscheinen?';
+$lang['Header'] = '(Default Wert für Option "header) Wie soll der Kopf bei Listen beschriftet werden? Als "id" oder mit der ersten Kopfzeile der Seite "firstheader".';
+$lang['Username'] = '(Default Wert für Option "username") Wie soll der Username ausgegeben werden? Als "username", voller Name "real" oder garnicht "none"';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
