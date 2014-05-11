017af253468915c41daf763e5b70f0c095677bc7
runout-at <markus@runout.at>
2014-05-11 23:19:10 +0200

    Update metadata.php
    new options

--- conf/metadata.php
+++ conf/metadata.php
@@ -10,5 +10,8 @@ $meta['AllowLinks'] = array('onoff');
 $meta['ActionNamespace'] = array('string');
 $meta['Strikethrough'] = array('onoff');
 $meta['CheckboxText'] = array('onoff');
+$meta['Checkbox'] = array('onoff');
+$meta['Header'] = array('multichoice', '_choices' => array('id','firstheader'));
+$meta['Username'] = array('multichoice', '_choices' => array('user','real','none'));
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
