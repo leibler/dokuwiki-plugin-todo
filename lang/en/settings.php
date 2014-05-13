417ccbc680399f6aa40e8a79129a969e9c1f0692
runout-at <markus@runout.at>
2014-05-13 17:53:58 +0200

    Update settings.php
    new value 'none' for $lang['Header']

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -10,7 +10,7 @@ $lang['ActionNamespace'] = 'What namespace should your actions be created in (".
 $lang['Strikethrough'] = 'Should the actions have strikethrough applied when checked?';
 $lang['CheckboxText'] = 'If AllowLinks is disabled, should clicking the actions\' text mark the action complete?';
 $lang['Checkbox'] = '(Default value for option "checkbox") Should the CheckBox be rendered in a list view?';
-$lang['Header'] = '(Default value for option "header") How should the header of a list be named? As "id" or as the first header of the page "firstheader".';
+$lang['Header'] = '(Default value for option "header") How should the header of a list be named? As "id" or as the first header of the page "firstheader" or no header at all "none".';
 $lang['Username'] = '(Default value for option "username") How should the name of the assigned user be rendered? As "username", full name "real" oder not at all "none"';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
