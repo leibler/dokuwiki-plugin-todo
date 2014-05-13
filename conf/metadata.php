6588f74ac806bfe39a0b5f1d98f50ddc5184adb3
runout-at <markus@runout.at>
2014-05-13 17:51:32 +0200

    Update metadata.php
    new value for $meta['Header']

--- conf/metadata.php
+++ conf/metadata.php
@@ -11,7 +11,7 @@ $meta['ActionNamespace'] = array('string');
 $meta['Strikethrough'] = array('onoff');
 $meta['CheckboxText'] = array('onoff');
 $meta['Checkbox'] = array('onoff');
-$meta['Header'] = array('multichoice', '_choices' => array('id','firstheader'));
+$meta['Header'] = array('multichoice', '_choices' => array('id','firstheader','none'));
 $meta['Username'] = array('multichoice', '_choices' => array('user','real','none'));
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
