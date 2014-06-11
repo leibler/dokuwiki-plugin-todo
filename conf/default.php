4298290ebde5effd07ba580110820a6e4dca74c4
runout-at <markus@runout.at>
2014-06-11 03:13:29 +0200

    seperate showdate tag/list

--- conf/default.php
+++ conf/default.php
@@ -10,5 +10,7 @@ $conf['CheckboxText'] = 1; //Should we allow action text to check the checkbox
 $conf['Checkbox'] = 1; // Should the Checkbox be rendered in list view
 $conf['Header'] = 'id'; // How should the header of list be rendered ID/FIRSTHEADER
 $conf['Username'] = 'user'; //How should the name of the assigned user be rendered USER/REALNAME/NONE
-$conf['Showdate'] = 0; // Should the Start/Due-Date be rendered
+$conf['ShowdateTag'] = 1; // Should the Start/Due-Date be rendered in a tag
+$conf['ShowdateList'] = 0; // Should the Start/Due-Date be rendered in list view
+
 //Setup VIM: ex: et ts=2 enc=utf-8 :
