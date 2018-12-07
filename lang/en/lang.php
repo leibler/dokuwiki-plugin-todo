fc89f2d0f06f90ca004210c7386922b9845d00a2
Blair <blairmccaughey@aol.com>
2018-12-07 21:10:39 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,17 +1,32 @@
 <?php
+
 /**
  * English language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Babbage <babbage@digitalbrink.com>
  * @date 20140317 Leo Eibler <dokuwiki@sprossenwanne.at> \n
- *                replace 'checkboxchange' with 'checkboxchange_on' and 'checkboxchange_off'  \n
+ * replace 'checkboxchange' with 'checkboxchange_on' and 'checkboxchange_off'  \n
+ *
+ * @author Blair <blairmccaughey@aol.com>
+ * @author Babbage <babbage@digitalbrink.com>
  */
- 
-// custom language strings for the plugin
-$lang['qb_todobutton'] = 'Mark text as ToDo';
-$lang['refreshpage'] = 'A newer version of this page is available, refresh your page before trying again.';
-$lang['checkboxchange_on'] = 'ToDo checked';
-$lang['checkboxchange_off'] = 'ToDo unchecked';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['qb_todobutton']         = 'casino slots
+online casino gambling
+slot game
+casino games slots
+casino games slots';
+$lang['refreshpage']           = 'casino slots
+online casino gambling
+slot game
+casino games slots
+casino games slots';
+$lang['checkboxchange_on']     = 'casino slots
+online casino gambling
+slot game
+casino games slots
+casino games slots';
+$lang['checkboxchange_off']    = 'casino slots
+online casino gambling
+slot game
+casino games slots
+casino games slots';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,17 +1,31 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * English language strings for the ToDo Plugin
  *
- * @author  Babbage <babbage@digitalbrink.com>
+ * @author Blair <blairmccaughey@aol.com>
+ * @author Babbage <babbage@digitalbrink.com>
  */
- 
-$lang['AllowLinks'] = 'Allow actions to also link to pages with the same name?';
-$lang['ActionNamespace'] = 'What namespace should your actions be created in (".:" = Current NS, Blank = Root NS)';
-$lang['Strikethrough'] = 'Should the actions have strikethrough applied when checked?';
-$lang['CheckboxText'] = 'If AllowLinks is disabled, should clicking the actions\' text mark the action complete?';
-$lang['Checkbox'] = '(Default value for option "checkbox") Should the CheckBox be rendered in a list view?';
-$lang['Header'] = '(Default value for option "header") How should the header of a list be named? As "id" or as the first header of the page "firstheader" or no header at all "none".';
-$lang['Username'] = '(Default value for option "username") How should the name of the assigned user be rendered? As "username", full name "real" oder not at all "none"';
-$lang['ShowdateTag'] = '(Default value for option "showdate") Should the Start/Due-date be rendered in tag definition view?';
-$lang['ShowdateList'] = '(Default value for option "showdate") Should the Start/Due-date be rendered in list view?';
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['AllowLinks']            = 'McCaughey';
+$lang['ActionNamespace']       = 'McCaughey';
+$lang['Strikethrough']         = 'casino slots
+online casino gambling
+slot game
+casino games slots
+casino games slots';
+$lang['CheckboxText']          = 'casino slots
+online casino gambling
+slot game
+casino games slots
+casino games slots';
+$lang['Checkbox']              = 'casino slots
+online casino gambling
+slot game
+casino games slots
+casino games slots';
+$lang['Header']                = 'casino slots';
+$lang['Username']              = 'McCaughey';
+$lang['ShowdateTag']           = '19';
+$lang['ShowdateList']          = '09172 25 28 58';
