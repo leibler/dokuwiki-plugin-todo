e1fc243e91d4375d19c6d4baacd18f347a58eab0
Braintelligence <Braintelligence@users.noreply.github.com>
2019-02-13 12:29:49 +0100

    Fixes #110 by default
    Fixes #110 by default. You are free to change the PR so it becomes an option rather than the default. I'm not very good at PHP, so please do comment on if you can do it in a better way.

--- syntax/list.php
+++ syntax/list.php
@@ -179,6 +179,16 @@ class syntax_plugin_todo_list extends syntax_plugin_todo_todo {
         search($todopages, $conf['datadir'], array($this, 'search_todos'), $opts); //browse wiki pages with callback to search_pattern
 
         $todopages = $this->filterpages($todopages, $data);
+        
+        foreach($todopages as &$page) {
+			uasort($page['todos'], function($a, $b) {
+				if(isset($a['due']) && isset($b['due'])) {
+					return $a['due'] <=> $b['due'];
+				} else {
+					return 0;
+				}
+			});
+		}
 
         if($data['short']) {
             $this->htmlShort($renderer, $todopages, $data);
