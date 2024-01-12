2c3eb88b5d97a7836ebba6a935f8bce9420f389e
k04an <ne.k04an@ya.ru>
2024-01-12 08:55:37 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author serg <sergey_art82@inbox.ru>
  */
 $lang['qb_todobutton']         = 'Отметить текст как Задача (ToDo)';
