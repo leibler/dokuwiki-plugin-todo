7bd7946de828214440284299d31f6cb232eb47bc
Tiberiu Popescu <popescu.tibi@gmail.com>
2025-09-03 10:50:09 +0200

    translation update

---
+++ lang/ro/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tiberiu Popescu <popescu.tibi@gmail.com>
+ */
+$lang['qb_todobutton']         = 'Marchează textul ca ToDo';
+$lang['refreshpage']           = 'O nouă versiune a paginii este disponibilă, reîncarcă pagina înainte de a încerca din nou.';
+$lang['checkboxchange_on']     = 'Todo bifat';
+$lang['checkboxchange_off']    = 'ToDo nebifat';
+$lang['uncheckall_todos']      = 'Debifează toate ToDo';
