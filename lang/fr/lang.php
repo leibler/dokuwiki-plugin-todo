1c7f574370460177ed557105a5f273e72b2583b5
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2025-09-20 17:25:20 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,10 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  */
 $lang['qb_todobutton']         = 'Marquer le texte comme tâche.';
 $lang['refreshpage']           = 'Une nouvelle révision de cette page est disponible. Veuillez rafraîchir la page avant d\'essayer à nouveau.';
 $lang['checkboxchange_on']     = 'tâche cochée';
 $lang['checkboxchange_off']    = 'tâche décochée';
+$lang['uncheckall_todos']      = 'dé-cocher toutes les tâches';
