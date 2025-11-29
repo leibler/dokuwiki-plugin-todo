3c8debbbb941eef7ad3b372eb1419e7c7fb6aad5
Michael Jahn <michael.jahn1990@gmail.com>
2025-11-29 21:17:28 +0100

    Translation update (de)

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,20 +1,21 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * English language strings for the ToDo Plugin
  *
- * @author  Leo Eibler <dokuwiki@sprossenwanne.at>
  * @date 20130405 Leo Eibler <dokuwiki@sprossenwanne.at> \n
- *                create german language file \n
+ * create german language file \n
+ *
+ * @author Leo Eibler <dokuwiki@sprossenwanne.at>
  */
- 
-$lang['AllowLinks'] = 'Sollen Aufgaben/Todos auch auf Seiten mit dem gleichen Namen verlinken?';
-$lang['ActionNamespace'] = 'Wenn AllowLinks aktiv ist, mit welchem Namespace sollen die Aufgaben-Seiten erstellt werden (".:" = Aktueller NS, leer = Root NS)';
-$lang['Strikethrough'] = 'Sollen erledigte Aufgaben durchgestrichen werden?';
-$lang['CheckboxText'] = 'Wenn AllowLinks nicht aktiv ist, soll dann ein Klick auf die Aufgabe, die Aufgabe als erledigt markieren?';
-$lang['Checkbox'] = '(Default Wert für Option "checkbox") Soll in Listen die CheckBox aufscheinen?';
-$lang['Header'] = '(Default Wert für Option "header) Wie soll der Kopf bei Listen beschriftet werden? Als "id" oder mit der ersten Kopfzeile der Seite "firstheader" oder ohne Kopfzeile "none"".';
-$lang['Username'] = '(Default Wert für Option "username") Wie soll der Username ausgegeben werden? Als "username", voller Name "real" oder garnicht "none"';
-$lang['ShowdateTag'] = '(Default Wert für Option "showdate") Soll das Start/Fällig-Datum bei Tag-Definitionen ausgegeben werden?';
-$lang['ShowdateList'] = '(Default Wert für Option "showdate") Soll das Start/Fällig-Datum bei Listen ausgegeben werden?';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['AllowLinks']            = 'Sollen Aufgaben/Todos auch auf Seiten mit dem gleichen Namen verlinken?';
+$lang['ActionNamespace']       = 'Wenn AllowLinks aktiv ist, mit welchem Namespace sollen die Aufgaben-Seiten erstellt werden (".:" = Aktueller NS, leer = Root NS)';
+$lang['Strikethrough']         = 'Sollen erledigte Aufgaben durchgestrichen werden?';
+$lang['CheckboxText']          = 'Wenn AllowLinks nicht aktiv ist, soll dann ein Klick auf die Aufgabe, die Aufgabe als erledigt markieren?';
+$lang['Checkbox']              = '(Default Wert für Option "checkbox") Soll in Listen die CheckBox aufscheinen?';
+$lang['Header']                = '(Default Wert für Option "header) Wie soll der Kopf bei Listen beschriftet werden? Als "id" oder mit der ersten Kopfzeile der Seite "firstheader" oder ohne Kopfzeile "none"".';
+$lang['Username']              = '(Default Wert für Option "username") Wie soll der Username ausgegeben werden? Als "username", voller Name "real" oder garnicht "none"';
+$lang['ShowdateTag']           = '(Default Wert für Option "showdate") Soll das Start/Fällig-Datum bei Tag-Definitionen ausgegeben werden?';
+$lang['ShowdateList']          = '(Default Wert für Option "showdate") Soll das Start/Fällig-Datum bei Listen ausgegeben werden?';
