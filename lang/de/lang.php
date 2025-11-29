48f476b4a19e3c427641c29a834bff581b8dde9c
Michael Jahn <michael.jahn1990@gmail.com>
2025-11-29 21:17:17 +0100

    Translation update (de)

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,19 +1,19 @@
 <?php
+
 /**
  * German language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Leo Eibler <dokuwiki@sprossenwanne.at>
  * @date 20130405 Leo Eibler <dokuwiki@sprossenwanne.at> \n
- *                create german language file \n
+ * create german language file \n
  * @date 20140317 Leo Eibler <dokuwiki@sprossenwanne.at> \n
- *                add 'refreshpage', 'checkboxchange_on' and 'checkboxchange_off'  \n
+ * add 'refreshpage', 'checkboxchange_on' and 'checkboxchange_off'  \n
+ *
+ * @author Michael Jahn <michael.jahn1990@gmail.com>
+ * @author Leo Eibler <dokuwiki@sprossenwanne.at>
  */
- 
-// custom language strings for the plugin
-$lang['qb_todobutton'] = 'Markierten Text als Aufgabe/Todo';
-$lang['refreshpage'] = 'Eine neue Version dieser Seite ist verfügbar. Bitte laden Sie die Seite neu.';
-$lang['checkboxchange_on'] = 'ToDo erledigt';
-$lang['checkboxchange_off'] = 'ToDo unerledigt';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['qb_todobutton']         = 'Markierten Text als Aufgabe/Todo';
+$lang['refreshpage']           = 'Eine neue Version dieser Seite ist verfügbar. Bitte laden Sie die Seite neu.';
+$lang['checkboxchange_on']     = 'ToDo erledigt';
+$lang['checkboxchange_off']    = 'ToDo unerledigt';
+$lang['uncheckall_todos']      = 'Alle Aufgaben abhaken';
