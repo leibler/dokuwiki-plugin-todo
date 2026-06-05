eeda848a98d43840fbfeebc642d593666c167258
Wizzard <wizzardsk@gmail.com>
2026-06-05 20:12:11 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['AllowLinks']            = 'Povoliť, aby úlohy zároveň odkazovali na stránky s rovnakým názvom?';
+$lang['ActionNamespace']       = 'V akom mennom priestore sa majú úlohy vytvárať (".:" = aktuálny, prázdne = koreňový)';
+$lang['Strikethrough']         = 'Majú byť úlohy po označení prečiarknuté?';
+$lang['CheckboxText']          = 'Ak je AllowLinks vypnuté, má kliknutie na text úlohy označiť úlohu ako dokončenú?';
+$lang['Checkbox']              = '(Predvolená hodnota voľby "checkbox") Má sa v zozname zobrazovať checkbox?';
+$lang['Header']                = '(Predvolená hodnota voľby "header") Ako sa má nazvať hlavička zoznamu? Ako "id", ako prvý nadpis stránky "firstheader", alebo bez hlavičky "none".';
+$lang['Username']              = '(Predvolená hodnota voľby "username") Ako sa má zobrazovať meno prideleného používateľa? Ako "username", celé meno "real", alebo vôbec "none"';
+$lang['ShowdateTag']           = '(Predvolená hodnota voľby "showdate") Má sa dátum začiatku/termínu zobrazovať v zobrazení definície tagu?';
+$lang['ShowdateList']          = '(Predvolená hodnota voľby "showdate") Má sa dátum začiatku/termínu zobrazovať v zozname?';
